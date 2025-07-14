4e1b8df231aac42c3d53bbf5d089c7f7de056775
Axel <axel.vergara99@gmail.com>
2025-07-13 21:23:44 -0400

    Se corrigieron archivos por falla en funcionalidad de logins

--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
 
 INSERT INTO usuarios (correo, clave) VALUES (
     'admin@ecodatos.com',
-    '$2y$10$KbQiN7WfLv4AqIFa7x/kRO1U9/xMZicn4G8HJfYylzO5qQ4E8ZqkC'
+    '$2y$10$Ly6kyGjGO80cXeibLd7aauli.EKZRn1ogrM9A/kGUXh/gOiaQSnRS'
 )
 ON CONFLICT (correo) DO NOTHING;
 
---
+++ frontend/README.md
@@ -0,0 +1,6 @@
+## ECODATOS - Proyecto con php y docker
+
+# Requisitos
+- docker instalado
+- docker compose instalado
+etc etc etc
